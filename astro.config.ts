2c54d6dec3af49f14e17569eb72e5747a10243bf
annaadar <anna.adar270@gmail.com>
2026-05-03 23:48:12 +0300

    Revert "fix: fix icons"
    This reverts commit 6497a91fdf990bb6836f2877d09bd4f30d0594a4.

--- astro.config.ts
+++ astro.config.ts
@@ -45,12 +45,12 @@ export default defineConfig({
 					type: "image/png",
 				},
 				{
-					src: "/web-app-manifest-192x192.png",
+					src: "/icon-192.png",
 					sizes: "192x192",
 					type: "image/png",
 				},
 				{
-					src: "/web-app-manifest-512x512.png",
+					src: "/icon-512.png",
 					sizes: "512x512",
 					type: "image/png",
 				},
