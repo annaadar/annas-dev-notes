2ce7dfb691539e544191c6c190b30822d2ddb0b9
annaadar <anna.adar270@gmail.com>
2026-05-03 22:33:40 +0300

    fix: remove /icons directory

--- astro.config.ts
+++ astro.config.ts
@@ -40,17 +40,17 @@ export default defineConfig({
 			icon: "public/icon.svg", // the source for generating favicon & icons
 			icons: [
 				{
-					src: "icons/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
+					src: "/apple-touch-icon.png", // used in src/components/BaseHead.astro L:26
 					sizes: "180x180",
 					type: "image/png",
 				},
 				{
-					src: "icons/icon-192.png",
+					src: "/icon-192.png",
 					sizes: "192x192",
 					type: "image/png",
 				},
 				{
-					src: "icons/icon-512.png",
+					src: "/icon-512.png",
 					sizes: "512x512",
 					type: "image/png",
 				},
